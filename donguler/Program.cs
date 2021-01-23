f2f3de914f2ce52c15ddf3e1cff047c9f5386b72
İsmail Kundakcı <kndkismail@gmail.com>
2021-01-23 17:34:20 +0300

    .

---
+++ donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Proglamlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            // array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Proglamlamaya başlangıç için temel kurs", "Java" , "python" , "C#" };
+
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+            // foreach - dizileri tek tek dönmeye yarar54
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ kampIntroo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace kampIntroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety - tip güvenliği
+            // do not repeat yourself - kendini tekrarlama
+            // değer tutucu -  alias    
+            // double = ondalıklı sayılar için
+            // integer(int) = tam sayı
+            // if = bir şart durumu vardır şart geçerli ise bu çalışır
+            // else = şart geçerli değil ise bu çalışır
+
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");  
+            }
+
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
