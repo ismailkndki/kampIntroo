7cebefd1319879960d1ab9275bcf10aa3b72e7e5
ismail kundakcı <kndkismail@gmail.com>
2021-01-25 20:53:07 +0300

    20 ocak Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        // ürün ismi
+        public int Id { get; set; }
+        // hangi kategoriye dahil olduğu
+        public int CategoryId { get; set; }
+        //ürün ismi
+        public string ProductName { get; set; }
+        //ürünün birim fiyatı
+        public double UnitPrice { get; set; }
+        //ürünün stok adedi
+        public int UnitInStock { get; set; }
+    }
+}
